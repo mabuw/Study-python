3a1024e78ab075c50a4b926b143f2901c6eb2fd9
mabuw <sonnguyen23476@gmail.com>
2021-08-12 23:38:56 +0700

    first commit

---
+++ unit5.py
@@ -0,0 +1,16 @@
+#Boolean / Algebra Logic ( Đại số Logic )
+
+if age >= 15 and age <= 17: #2 dấu mầu đỏ được gọi là 2 mệnh đề
+#Cả 2 mệnh đề đều có thể đúng hoặc sai ( True / False )
+#True and True = True
+#True and False = False
+#False and True = False
+
+if age >= 15 or age <= 17
+#True or False = True
+#False or True = True
+#True or True = True
+#False or False = False
+
+#Đối với mệnh đề và thì cả 2 cái cần đúng thì mệnh đề mới chạy
+#Đối với mệnh đề hoặc thì 1 trong 2 cái đúng là chương trình sẽ chạy
